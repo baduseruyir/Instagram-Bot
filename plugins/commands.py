3b6961e0014ca3a97f5dc476b513d446809369bd
Badu_Ser <100788930+baduseruyir@users.noreply.github.com>
2022-03-09 18:11:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -91,14 +91,14 @@ async def help(bot, cmd):
 		reply_markup=InlineKeyboardMarkup(
 			[
 				[
-					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-					InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122"),
-					InlineKeyboardButton("⚙️Update Channel", url="https://t.me/subin_works")
+					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/baduser'),
+					InlineKeyboardButton("🤖Other Bots", url="https://t.me/baduser"),
+					InlineKeyboardButton("⚙️Update Channel", url="https://t.me/status_world_malayalam")
 					
 				],
 				[
-					InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-					InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/subinps/Instagram-Bot")
+					InlineKeyboardButton("🔗Source Code", url="https://github.com/baduseruyir/Instagram-Bot"),
+					InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/baduseruyir/Instagram-Bot")
 				]
 			]
 			)
@@ -117,7 +117,7 @@ async def stop(bot, cmd):
 						InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")	
 					],
                     [
-                        InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
+                        InlineKeyboardButton("🔗Source Code", url="https://github.com/baduseruyir/Instagram-Bot"),
 						InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/subinps/Instagram-Bot")
                     ],
                     [
